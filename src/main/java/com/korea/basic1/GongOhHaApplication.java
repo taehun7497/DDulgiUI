8a1a5bffc369524998d51084984ba207d6a6ca87
Dowon Nam <dwnam0114@gmail.com>
2024-04-29 14:58:03 +0900

    start

---
+++ src/main/java/com/korea/basic1/GongOhHaApplication.java
@@ -0,0 +1,13 @@
+package com.korea.basic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongOhHaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongOhHaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korea/basic1/GongOhHaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korea.basic1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongOhHaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
