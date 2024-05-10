fc347c6a5a3cb99dec99520b442fa7f8557bd0fa
taehun7497 <xogns7497@naver.com>
2024-05-10 10:02:30 +0900

    whyrano

--- src/main/java/com/korea/basic1/dulgiUIApplication.java
+++ src/main/java/com/korea/basic1/dulgiUIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GongOhHaApplication {
+public class dulgiUIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GongOhHaApplication.class, args);
+		SpringApplication.run(dulgiUIApplication.class, args);
 	}
 
 }
